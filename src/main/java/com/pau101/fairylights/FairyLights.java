0ec2aad3586b421daa335e70494bbf53bca5f3ae
pau101 <paul.fulham0@gmail.com>
2017-01-04 00:16:02 -0800

    Release 2.0.5

--- src/main/java/com/pau101/fairylights/FairyLights.java
+++ src/main/java/com/pau101/fairylights/FairyLights.java
@@ -21,7 +21,7 @@ import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 	modid = FairyLights.ID,
 	name = FairyLights.NAME,
 	version = FairyLights.VERSION,
-	dependencies = "required-after:forge@[13.19.0.2153,);",
+	dependencies = "required-after:forge@[13.19.1.2199,);",
 	guiFactory = "com.pau101.fairylights.client.gui.FairyLightsGuiFactory"
 )
 public final class FairyLights {
@@ -29,7 +29,7 @@ public final class FairyLights {
 
 	public static final String NAME = "Fairy Lights";
 
-	public static final String VERSION = "2.0.4";
+	public static final String VERSION = "2.0.5";
 
 	@Instance(ID)
 	public static FairyLights instance;
