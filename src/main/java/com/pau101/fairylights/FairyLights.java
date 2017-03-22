fbc97c8bf6b0852cbc6a322bae82220f36900e61
pau101 <paul.fulham0@gmail.com>
2017-03-22 12:53:01 -0700

    Release 2.1.0

--- src/main/java/com/pau101/fairylights/FairyLights.java
+++ src/main/java/com/pau101/fairylights/FairyLights.java
@@ -29,7 +29,7 @@ public final class FairyLights {
 
 	public static final String NAME = "Fairy Lights";
 
-	public static final String VERSION = "2.0.7";
+	public static final String VERSION = "2.1.0";
 
 	@Instance(ID)
 	public static FairyLights instance;
