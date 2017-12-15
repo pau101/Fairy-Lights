766e4c16d14e88763ac27a3fe885992f7a1cef63
Paul Fulham <paul.fulham0@gmail.com>
2017-12-14 17:58:07 -0800

    Fix jingler key

--- src/main/java/com/pau101/fairylights/client/midi/MidiJingler.java
+++ src/main/java/com/pau101/fairylights/client/midi/MidiJingler.java
@@ -30,16 +30,14 @@ public final class MidiJingler implements Receiver {
 		if (shortMsg.getCommand() != ShortMessage.NOTE_ON || shortMsg.getData2() == 0) {
 			return;
 		}
-		int inMCSpace = shortMsg.getData1() - 54; // F#3 is 0
+		int inMCSpace = shortMsg.getData1() - 53; // F#3 is 0
 		// Keep F#5 the same
 		int note = inMCSpace == 24 ? 24 : Mth.mod(inMCSpace, 24);
 		Light[] lights = connection.getFeatures();
 		int offset = lights.length / 2 - 12;
 		int idx = note + offset;
 		if (idx >= 0 && idx < lights.length) {
-			Minecraft.getMinecraft().addScheduledTask(() -> {
-				lights[idx].jingle(connection.getWorld(), connection.getFastener().getConnectionPoint(), note);
-			});
+			Minecraft.getMinecraft().addScheduledTask(() -> lights[idx].jingle(connection.getWorld(), connection.getFastener().getConnectionPoint(), note));
 		}
 	}
 
