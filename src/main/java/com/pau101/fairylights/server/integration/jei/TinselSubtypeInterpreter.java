ba64fc0a618ed60dd4563eb14f01df9f8c927c22
Paul Fulham <paul.fulham0@gmail.com>
2018-11-21 08:45:11 -0800

    Update forge version

--- src/main/java/com/pau101/fairylights/server/integration/jei/TinselSubtypeInterpreter.java
+++ src/main/java/com/pau101/fairylights/server/integration/jei/TinselSubtypeInterpreter.java
@@ -1,7 +1,5 @@
 package com.pau101.fairylights.server.integration.jei;
 
-import javax.annotation.Nullable;
-
 import mezz.jei.api.ISubtypeRegistry.ISubtypeInterpreter;
 import net.minecraft.item.EnumDyeColor;
 import net.minecraft.item.ItemStack;
@@ -9,13 +7,12 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.common.util.Constants.NBT;
 
 public final class TinselSubtypeInterpreter implements ISubtypeInterpreter {
-	@Nullable
 	@Override
-	public String getSubtypeInfo(ItemStack stack) {
+	public String apply(ItemStack stack) {
 		NBTTagCompound compound = stack.getTagCompound();
 		if (compound != null && compound.hasKey("color", NBT.TAG_BYTE)) {
 			return EnumDyeColor.byDyeDamage(compound.getByte("color")).getName();
 		}
-		return null;
+		return NONE;
 	}
 }
