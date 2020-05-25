d6d878a46c812a97e59b9badab95a970d4bbe068
Paul Fulham <paul.fulham0@gmail.com>
2020-05-25 02:30:53 -0700

    Reintroduce garland bow

---
+++ src/main/java/me/paulf/fairylights/client/model/light/BowModel.java
@@ -0,0 +1,51 @@
+package me.paulf.fairylights.client.model.light;
+
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.model.Model;
+import net.minecraft.client.renderer.model.ModelRenderer;
+
+public class BowModel extends Model {
+    private final ModelRenderer root;
+
+    public BowModel() {
+        super(RenderType::getEntityCutout);
+        this.textureWidth = 128;
+        this.textureHeight = 128;
+        this.root = new ModelRenderer(this, 6, 72);
+        this.root.setRotationPoint(0.0F, 0.5F, -3.25F);
+        this.root.addBox(-2.0F, -1.5F, -1.0F, 4.0F, 3.0F, 2.0F, 0.0F, false);
+        final ModelRenderer bone = new ModelRenderer(this, 0, 77);
+        bone.setRotationPoint(-1.0F, 1.0F, 0.0F);
+        setRotationAngle(bone, 0.0F, 0.1745F, -0.5236F);
+        bone.addBox(-5.0F, -4.0F, 0.0F, 5.0F, 5.0F, 1.0F, 0.0F, false);
+        this.root.addChild(bone);
+        final ModelRenderer bone2 = new ModelRenderer(this, 0, 77);
+        bone2.setRotationPoint(1.0F, 1.0F, 0.0F);
+        setRotationAngle(bone2, 0.0F, -0.1745F, 0.5236F);
+        bone2.addBox(0.0F, -4.0F, 0.0F, 5.0F, 5.0F, 1.0F, 0.0F, true);
+        this.root.addChild(bone2);
+        final ModelRenderer bone3 = new ModelRenderer(this, 0, 72);
+        bone3.setRotationPoint(0.0F, -1.0F, 0.0F);
+        setRotationAngle(bone3, 0.0873F, 0.0873F, -0.1745F);
+        bone3.addBox(-2.0F, -4.0F, 0.0F, 2.0F, 4.0F, 1.0F, 0.0F, false);
+        this.root.addChild(bone3);
+        final ModelRenderer bone4 = new ModelRenderer(this, 0, 72);
+        bone4.setRotationPoint(0.0F, -1.0F, 0.0F);
+        setRotationAngle(bone4, 0.0873F, -0.0873F, 0.1745F);
+        bone4.addBox(0.0F, -4.0F, 0.0F, 2.0F, 4.0F, 1.0F, 0.0F, true);
+        this.root.addChild(bone4);
+    }
+
+    @Override
+    public void render(final MatrixStack matrix, final IVertexBuilder builder, final int light, final int overlay, final float r, final float g, final float b, final float a) {
+        this.root.render(matrix, builder, light, overlay, r, g, b, a);
+    }
+
+    private static void setRotationAngle(final ModelRenderer renderer, final float x, final float y, final float z) {
+        renderer.rotateAngleX = x;
+        renderer.rotateAngleY = y;
+        renderer.rotateAngleZ = z;
+    }
+}
--- src/main/java/me/paulf/fairylights/client/renderer/block/entity/FastenerRenderer.java
+++ src/main/java/me/paulf/fairylights/client/renderer/block/entity/FastenerRenderer.java
@@ -1,6 +1,9 @@
 package me.paulf.fairylights.client.renderer.block.entity;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
+import me.paulf.fairylights.client.ClientProxy;
+import me.paulf.fairylights.client.model.light.BowModel;
 import me.paulf.fairylights.server.fastener.Fastener;
 import me.paulf.fairylights.server.fastener.connection.type.Connection;
 import me.paulf.fairylights.server.fastener.connection.type.garland.GarlandTinselConnection;
@@ -9,6 +12,9 @@ import me.paulf.fairylights.server.fastener.connection.type.hanginglights.Hangin
 import me.paulf.fairylights.server.fastener.connection.type.letter.LetterBuntingConnection;
 import me.paulf.fairylights.server.fastener.connection.type.pennant.PennantBuntingConnection;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.Vector3f;
+import net.minecraft.util.Direction;
 
 public class FastenerRenderer {
     private final HangingLightsRenderer hangingLights = new HangingLightsRenderer();
@@ -16,8 +22,10 @@ public class FastenerRenderer {
     private final GarlandTinselRenderer tinsel = new GarlandTinselRenderer();
     private final PennantBuntingRenderer pennants = new PennantBuntingRenderer();
     private final LetterBuntingRenderer letters = new LetterBuntingRenderer();
+    private final BowModel bow = new BowModel();
 
     public void render(final Fastener<?> fastener, final float delta, final MatrixStack matrix, final IRenderTypeBuffer source, final int packedLight, final int packedOverlay) {
+        boolean renderBow = true;
         for (final Connection conn : fastener.getConnections().values()) {
             if (conn.isOrigin()) {
                 if (conn instanceof HangingLightsConnection) {
@@ -31,6 +39,14 @@ public class FastenerRenderer {
                 } else if (conn instanceof LetterBuntingConnection) {
                     this.letters.render((LetterBuntingConnection) conn, delta, matrix, source, packedLight, packedOverlay);
                 }
+            }
+            if (renderBow && conn instanceof GarlandVineConnection && fastener.getFacing().getAxis() != Direction.Axis.Y) {
+                final IVertexBuilder buf = ClientProxy.SOLID_TEXTURE.getBuffer(source, RenderType::getEntityCutout);
+                matrix.push();
+                matrix.rotate(Vector3f.YP.rotationDegrees(180.0F - fastener.getFacing().getHorizontalAngle()));
+                this.bow.render(matrix, buf, packedLight, packedOverlay, 1.0F, 1.0F, 1.0F, 1.0F);
+                matrix.pop();
+                renderBow = false;
             }
         }
     }
