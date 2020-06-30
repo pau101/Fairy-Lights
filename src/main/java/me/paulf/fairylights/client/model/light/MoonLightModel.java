f76ad0a6001692ba7a3f16905f11b4881c4a1017
Paul Fulham <paul.fulham0@gmail.com>
2020-06-29 17:08:25 -0700

    Refine moon and star models

--- src/main/java/me/paulf/fairylights/client/model/light/MoonLightModel.java
+++ src/main/java/me/paulf/fairylights/client/model/light/MoonLightModel.java
@@ -3,7 +3,7 @@ package me.paulf.fairylights.client.model.light;
 public class MoonLightModel extends ColorLightModel {
     public MoonLightModel() {
         int u = 76, v = 60;
-        this.unlit.setTextureOffset(u + 14, v + 0).addBox(-1.0F, 5.0F -7.0F, -1.0F, 2.0F, 3.0F, 2.0F);
+        this.unlit.setTextureOffset(u + 14, v + 0).addBox(-0.5F, 5.0F -7.0F, -0.5F, 1.0F, 3.0F, 1.0F);
         final BulbBuilder bulb = this.createBulb();
         bulb.setUV(u + 0, v + 0).addBox(0.0F, -1.0F -7.0F, -1.5F, 4.0F, 4.0F, 3.0F, 0.1F);
         bulb.setUV(u + 0, v + 7).addBox(-2.5F, 3.0F -7.0F, -1.5F, 5.0F, 2.0F, 3.0F);
--- src/main/java/me/paulf/fairylights/client/model/light/StarLightModel.java
+++ src/main/java/me/paulf/fairylights/client/model/light/StarLightModel.java
@@ -3,27 +3,24 @@ package me.paulf.fairylights.client.model.light;
 public class StarLightModel extends ColorLightModel {
     public StarLightModel() {
         final int u = 101, v = 73;
-        this.unlit.setTextureOffset(u + 8, v + 0).addBox(-0.5F, 5.0F-7.0f, -0.5F, 1.0F, 3.0F, 1.0F);
+        this.unlit.setTextureOffset(u + 12, v + 0).addBox(-0.5F, 5.0F-7.0f, -0.5F, 1.0F, 3.0F, 1.0F);
         final BulbBuilder bulb = this.createBulb();
         bulb.setPosition(0.0F, 1.0F-7.0f, 0.0F);
-        bulb.setUV(u, v).addBox(-1.0F, -1.0F, -1.0F, 2.0F, 2.0F, 2.0F, 0.2F);
-        bulb.setUV(u + 0, v + 5).addBox(-1.0F, 1.07F, -1.0F, 2.0F, 2.0F, 2.0F, 0.0F);
-        bulb.setUV(u + 0, v + 8).addBox(-0.5F, 3.035F, -1.0F, 1.0F, 1.0F, 2.0F, -0.035F);
+        final BulbBuilder center = bulb.createChild(u, v);
+        center.setAngles(0.0F, 0.0F, 0.7854F);
+        center.addBox(-2.0F, -2.0F, -1.0F, 4.0F, 4.0F, 2.0F, 0.15F);
+        bulb.setUV(u + 0, v + 4).addBox(-1.0F, 1.07F, -1.0F, 2.0F, 3.0F, 2.0F, 0.0F);
         final BulbBuilder b1 = bulb.createChild(u, v);
         b1.setAngles(0.0F, 0.0F, -1.2566F);
-        b1.setUV(u + 0, v + 4).addBox(-1.0F, 1.0F, -1.0F, 2.0F, 2.0F, 2.0F, 0.02F);
-        b1.setUV(u + 0, v + 8).addBox(-0.5F, 2.965F, -1.0F, 1.0F, 1.0F, 2.0F, -0.035F);
+        b1.setUV(u + 0, v + 4).addBox(-1.0F, 0.0F, -1.0F, 2.0F, 4.0F, 2.0F, 0.02F);
         final BulbBuilder b2 = bulb.createChild(u, v);
         b2.setAngles(0.0F, 0.0F, -2.5133F);
-        b2.setUV(u + 0, v + 4).addBox(-1.0F, 1.0F, -1.0F, 2.0F, 2.0F, 2.0F, 0.0F);
-        b2.setUV(u + 0, v + 8).addBox(-0.5F, 2.965F, -1.0F, 1.0F, 1.0F, 2.0F, -0.035F);
+        b2.setUV(u + 0, v + 4).addBox(-1.0F, 1.0F, -1.0F, 2.0F, 3.0F, 2.0F, 0.0F);
         final BulbBuilder b3 = bulb.createChild(u, v);
         b3.setAngles(0.0F, 0.0F, 2.5133F);
-        b3.setUV(u + 0, v + 4).addBox(-1.0F, 1.0F, -1.0F, 2.0F, 2.0F, 2.0F, 0.02F);
-        b3.setUV(u + 0, v + 8).addBox(-0.5F, 2.965F, -1.0F, 1.0F, 1.0F, 2.0F, -0.035F);
+        b3.setUV(u + 0, v + 4).addBox(-1.0F, 1.0F, -1.0F, 2.0F, 3.0F, 2.0F, 0.02F);
         final BulbBuilder b4 = bulb.createChild(u, v);
         b4.setAngles(0.0F, 0.0F, 1.2566F);
-        b4.setUV(u + 0, v + 4).addBox(-1.0F, 1.0F, -1.0F, 2.0F, 2.0F, 2.0F, -0.02F);
-        b4.setUV(u + 0, v + 8).addBox(-0.5F, 2.965F, -1.0F, 1.0F, 1.0F, 2.0F, -0.035F);
+        b4.setUV(u + 0, v + 4).addBox(-1.0F, 0.0F, -1.0F, 2.0F, 4.0F, 2.0F, -0.02F);
     }
 }
