42adb2bbd4cc31d2802cdfa0a24509ffe5844b51
Rubydesic <rubydesic@gmail.com>
2020-09-22 20:42:05 -0400

    Update VS integration (#89)

--- src/main/java/com/pau101/fairylights/server/integration/valkyrienskies/ValkyrienSkies.java
+++ src/main/java/com/pau101/fairylights/server/integration/valkyrienskies/ValkyrienSkies.java
@@ -8,10 +8,12 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import org.valkyrienskies.mod.common.entity.PhysicsWrapperEntity;
-import org.valkyrienskies.mod.common.math.Quaternion;
-import org.valkyrienskies.mod.common.physics.management.PhysicsObject;
-import org.valkyrienskies.mod.common.physmanagement.chunk.VSChunkClaim;
+import org.joml.AxisAngle4d;
+import org.joml.Quaterniond;
+import org.valkyrienskies.mod.common.ships.chunk_claims.VSChunkClaim;
+import org.valkyrienskies.mod.common.ships.ship_world.IHasShipManager;
+import org.valkyrienskies.mod.common.ships.ship_world.PhysicsObject;
+import org.valkyrienskies.mod.common.ships.ship_world.WorldClientShipManager;
 import valkyrienwarfare.api.IPhysicsEntity;
 import valkyrienwarfare.api.IPhysicsEntityManager;
 import valkyrienwarfare.api.TransformType;
@@ -19,21 +21,11 @@ import valkyrienwarfare.api.TransformType;
 public class ValkyrienSkies {
 	@SubscribeEvent
 	public static void onCollectFasteners(final CollectFastenersEvent event) {
-		event.getWorld().loadedEntityList.stream()
-			.filter(PhysicsWrapperEntity.class::isInstance)
-			.map(PhysicsWrapperEntity.class::cast)
-			.map(PhysicsWrapperEntity::getPhysicsObject)
+        WorldClientShipManager manager = ((WorldClientShipManager) ((IHasShipManager) event.getWorld()).getManager());
+        manager.getAllLoadedPhysObj()
 			.forEach(obj -> {
-				VSChunkClaim claim = obj.getOwnedChunks();
-				int minX = claim.minX();
-				int maxX = claim.maxX();
-				int minZ = claim.minZ();
-				int maxZ = claim.maxZ();
-				for (int x = minX; x < maxX; x++) {
-					for (int z = minZ; z < maxZ; z++) {
-						event.accept(obj.getChunkAt(x, z));
-					}
-				}
+				VSChunkClaim claim = obj.getChunkClaim();
+				claim.forEach((x, z) -> event.accept(obj.getChunkAt(x, z)));
 			});
 
 	}
@@ -69,21 +61,16 @@ public class ValkyrienSkies {
 			this.parent.unrotate(world, source, matrix, delta);
 			final PhysicsObject entity = (PhysicsObject) IPhysicsEntityManager.INSTANCE.getPhysicsEntityFromShipSpace(world, source);
 			if (entity != null) {
-				final Quaternion rot = getRotation(entity, delta);
-				final double[] radians = rot.toRadians();
-				final float pitch = (float) Math.toDegrees(radians[0]);
-				final float yaw = (float) Math.toDegrees(radians[1]);
-				final float roll = (float) Math.toDegrees(radians[2]);
-				matrix.rotate(pitch, 1.0F, 0.0F, 0.0F);
-				matrix.rotate(yaw, 0.0F, 1.0F, 0.0F);
-				matrix.rotate(roll, 0.0F, 0.0F, 1.0F);
+				final Quaterniond rot = getRotation(entity, delta);
+                AxisAngle4d rot2 = rot.get(new AxisAngle4d());
+				matrix.rotate((float) Math.toDegrees(rot2.angle), (float) rot2.x, (float) rot2.y, (float) rot2.z);
 			}
 		}
 
-		private Quaternion getRotation(final PhysicsObject object, final float delta) {
-			final Quaternion prevRotation = object.getShipTransformationManager().getPrevTickTransform().createRotationQuaternion(TransformType.GLOBAL_TO_SUBSPACE);
-			final Quaternion rotation = object.getShipTransformationManager().getCurrentTickTransform().createRotationQuaternion(TransformType.GLOBAL_TO_SUBSPACE);
-			return Quaternion.slerpInterpolate(prevRotation, rotation, delta);
+		private Quaterniond getRotation(final PhysicsObject object, final float delta) {
+			final Quaterniond prevRotation = object.getShipTransformationManager().getPrevTickTransform().rotationQuaternion(TransformType.GLOBAL_TO_SUBSPACE);
+			final Quaterniond rotation = object.getShipTransformationManager().getCurrentTickTransform().rotationQuaternion(TransformType.GLOBAL_TO_SUBSPACE);
+			return prevRotation.slerp(rotation, delta);
 		}
 	}
 }
