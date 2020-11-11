31bd802d43b440fa7979910bffd1d25988570cbe
Paul Fulham <paul.fulham0@gmail.com>
2020-11-10 20:25:26 -0800

    Reinstate light scheduling for #93

--- src/main/java/me/paulf/fairylights/server/connection/HangingLightsConnection.java
+++ src/main/java/me/paulf/fairylights/server/connection/HangingLightsConnection.java
@@ -21,16 +21,22 @@ import net.minecraft.nbt.ListNBT;
 import net.minecraft.util.Hand;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvent;
+import net.minecraft.util.Util;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.LightType;
 import net.minecraft.world.World;
 import net.minecraft.world.lighting.BlockLightEngine;
 import net.minecraft.world.lighting.IWorldLightListener;
+import net.minecraft.world.lighting.WorldLightManager;
+import net.minecraft.world.server.ServerWorldLightManager;
 import net.minecraftforge.common.util.Constants.NBT;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraftforge.items.ItemHandlerHelper;
 
 import javax.annotation.Nullable;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -223,11 +229,37 @@ public final class HangingLightsConnection extends HangingFeatureConnection<Ligh
         }
     }
 
+    private static final Method ADD_TASK;
+
+    private static final Object POST_PHASE;
+
+    static {
+        final Class<?> phaseType;
+        try {
+            phaseType = Class.forName("net.minecraft.world.server.ServerWorldLightManager$Phase");
+        } catch (final ClassNotFoundException e) {
+            throw new Error(e);
+        }
+        ADD_TASK = ObfuscationReflectionHelper.findMethod(ServerWorldLightManager.class, "func_215586_a", int.class, int.class, phaseType, Runnable.class);
+        POST_PHASE = phaseType.getEnumConstants()[1];
+    }
+
     private void setLight(final BlockPos pos) {
         if (this.world.isBlockPresent(pos) && this.world.isAirBlock(pos) && this.world.getLightFor(LightType.BLOCK, pos) < MAX_LIGHT) {
-            final IWorldLightListener light = this.world.getChunkProvider().getLightManager().getLightEngine(LightType.BLOCK);
+            final WorldLightManager manager = this.world.getChunkProvider().getLightManager();
+            final IWorldLightListener light = manager.getLightEngine(LightType.BLOCK);
             if (light instanceof BlockLightEngine) {
-                ((BlockLightEngine) light).func_215623_a(pos, MAX_LIGHT);
+                if (manager instanceof ServerWorldLightManager) {
+                    try {
+                        ADD_TASK.invoke(manager, pos.getX() >> 4, pos.getZ() >> 4, POST_PHASE, Util.namedRunnable(() -> {
+                            ((BlockLightEngine) light).func_215623_a(pos, MAX_LIGHT);
+                        }, () -> "setLight " + pos));
+                    } catch (final IllegalAccessException | InvocationTargetException e) {
+                        throw new RuntimeException(e);
+                    }
+                } else {
+                    ((BlockLightEngine) light).func_215623_a(pos, MAX_LIGHT);
+                }
             }
         }
     }
